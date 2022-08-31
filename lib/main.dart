37f496e428a57e92ad0bdf078cbb315f7a786b1d
Abhishek Pandey <88980729+Abhishekabh99@users.noreply.github.com>
2022-08-31 18:25:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:counter_app_getx/view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() => runApp(GetMaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
